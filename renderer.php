42af59175621fa781167d3fce92b6225935782b6
Kyle Temkin <ktemkin@binghamton.edu>
2012-11-01 18:51:30 -0400

    BU-78 optimized scriptedessay for scans

--- renderer.php
+++ renderer.php
@@ -34,8 +34,42 @@ require_once($CFG->dirroot.'/question/type/essay/renderer.php');
  * @copyright  2009 The Open University
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class qtype_scriptedessay_renderer extends qtype_essay_renderer 
-{
+class qtype_scriptedessay_renderer extends qtype_essay_renderer {
+
+    /**
+     * Displays any attached files when the question is in read-only mode.
+     * @param question_attempt $qa the question attempt to display.
+     * @param question_display_options $options controls what should and should
+     *      not be displayed. Used to get the context.
+     */
+    public function files_read_only(question_attempt $qa, question_display_options $options) {
+        $files = $qa->get_last_qt_files('attachments', $options->context->id);
+
+        $output = array();
+
+        print_object($files);
+
+        foreach ($files as $file) {
+
+            switch($file->get_mimetype()) {
+
+                case 'image/jpeg':
+                    $output[] = html_writer::tag('p', html_writer::empty_tag('img', array('src' => $qa->get_response_file_url($file))));
+                    break;
+
+                default:
+                    $output[] = html_writer::tag('p', html_writer::link($qa->get_response_file_url($file),
+                        $this->output->pix_icon(file_file_icon($file), get_mimetype_description($file),
+                        'moodle', array('class' => 'icon')) . ' ' . s($file->get_filename())));
+                    break;
+
+
+            }
+        }
+        return implode($output);
+    }
+
+
 }
 
 
